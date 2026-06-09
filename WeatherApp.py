c690417872c2a113e8ee64dff5c5e540b107dbdd
tsimonovic875 <tsimonovic875@gmail.com>
2026-06-09 16:14:56 +0200

    Prozor za aplikaciju

--- WeatherApp.py
+++ WeatherApp.py
@@ -1,1 +1,16 @@
+import tkinter as tk
+
+API_KEY = "06c921750b9a82d8f5d1294e1586276f"
+
+class AplikacijaVremena:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Aplikacija za prognozu vremena")
+        self.root.geometry("1000x650")
+        self.root.configure(bg="#1e293b")
+
+
+root = tk.Tk()
+app = AplikacijaVremena(root)
+root.mainloop()
 
